c3bb1cb648dcf494b6905d71527272dbe6a8060f
MichiY3 <michiyoy@hotmail.com>
2020-10-25 21:07:03 +0900

    HomeWork Q1-3

---
+++ script.js
@@ -0,0 +1,37 @@
+// このファイルに解答を書いてください。
+// 問題文はindex.htmlにあります。
+
+
+// Q 1　以下のテキストの文字を取得して、console.logで出力する
+let text = document.getElementById('q1-text');
+console.log(text);
+// Id の時は、getElement ←単数！！　
+
+
+// Q 2　以下の文字を任意の文字に変更する  →→→→→→textContent
+let text2 = document.getElementById('q2-text');
+console.log(text2);
+text2.textContent = "変更しました";
+
+
+// Q 3 以下の文字色を変更する →→→→→→style.color
+let text3 = document.getElementById('q3-text');
+console.log(text3);
+text3.style.color = '#00947a';
+
+
+// Q 4 (難) 以下の文字の色をすべて変更してください ３段
+// 「for of」を使います。
+
+ let text4 = document.getElementById('q4-text');
+ for (let text4 of 'q4-text');
+console.log(text4);
+ text4.style.color = '#ea5532';
+
+
+
+
+
+// Q 5   style.backgroundColor
+
+
